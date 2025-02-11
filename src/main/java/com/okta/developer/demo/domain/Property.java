73d8f60736fb3d926b4ac6afef3ed39c2c77b5e4
seungooan <wa594@naver.com>
2025-02-11 13:43:32 +0900

    [Refactor] 소셜 로그인 기능 추가

---
+++ src/main/java/com/okta/developer/demo/domain/Property.java
@@ -0,0 +1,75 @@
+package com.okta.developer.demo.domain;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Node
+public class Property {
+
+    @Id
+    @GeneratedValue  
+    private Long id;
+    private String address;
+    private String county;
+    private String district;
+    private String titleNumber;
+
+    public Property() {
+        this.id = null;
+    }
+
+    public Property(String address, String county, String district, String titleNumber) {
+        this.id = null;
+        this.address = address;
+        this.county = county;
+        this.district = district;
+        this.titleNumber = titleNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Property withId(Long id) {
+        Property newObject = new Property(this.address, this.county, this.district, this.titleNumber);
+        newObject.id = id;
+        return newObject;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getTitleNumber() {
+        return titleNumber;
+    }
+
+    public void setTitleNumber(String titleNumber) {
+        this.titleNumber = titleNumber;
+    }
+}
