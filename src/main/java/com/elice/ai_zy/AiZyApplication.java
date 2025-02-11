b7384b19538f5e80e9207658af6d772e45af25f5
SeungIlB <seoung1515@naver.com>
2025-02-11 13:07:42 +0900

    initial: 프로젝트 세팅-1차

---
+++ src/main/java/com/elice/ai_zy/AiZyApplication.java
@@ -0,0 +1,13 @@
+package com.elice.ai_zy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiZyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiZyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elice/ai_zy/AiZyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.ai_zy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiZyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
