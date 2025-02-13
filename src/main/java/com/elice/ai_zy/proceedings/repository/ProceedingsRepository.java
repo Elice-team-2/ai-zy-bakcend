aa596399e6c0b32aad326dd7141e12914fed62da
SeungIlB <seoung1515@naver.com>
2025-02-13 17:13:36 +0900

    Refactor: UUID를 String으로 변경

--- src/main/java/com/elice/ai_zy/proceedings/repository/ProceedingsRepository.java
+++ src/main/java/com/elice/ai_zy/proceedings/repository/ProceedingsRepository.java
@@ -7,4 +7,4 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface ProceedingsRepository extends JpaRepository<Proceedings, UUID> { }
+public interface ProceedingsRepository extends JpaRepository<Proceedings, String> {}
