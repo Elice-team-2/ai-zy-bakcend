ff85b4d1ee3cc53e5e9755b98c7c3f0c02622c7f
SeungIlB <seoung1515@naver.com>
2025-02-13 14:55:54 +0900

    Refactor: id 문자열로 변경 및 엔티티 수정

--- src/main/java/com/elice/ai_zy/proceedings/entity/Proceedings.java
+++ src/main/java/com/elice/ai_zy/proceedings/entity/Proceedings.java
@@ -1,36 +1,49 @@
 package com.elice.ai_zy.proceedings.entity;
 
 import com.elice.ai_zy.proceedings.dto.ProceedingsTags;
+import com.elice.ai_zy.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Proceedings {
     @Id
-    @GeneratedValue(generator = "proceedingsId")
-    @GenericGenerator(name = "proceedingsId", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(columnDefinition = "BINARY(16)", updatable = false, nullable = false)
-    private UUID id;
+    @GeneratedValue
+    @UuidGenerator
+    @Column(columnDefinition = "CHAR(36)", updatable = false, nullable = false)
+    private String proceedingsId;
+
 
     @Column(nullable = false, name = "title"  , length = 255, unique = true)
     private String title;
 
-    @Column(nullable = false, name = "contents", length = 255)
+    @Column(nullable = false, name = "contents", length = 30000)
     private String contents;
 
     @Column(nullable = true, name = "tags", length = 255)
     private ProceedingsTags tags;
 
-//    @Column(nullable = false, name = "attendeeName", length = 255)
-//    private User attendeeName;
-//    구현 필요
+//    @JoinColumn(nullable = true)
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner")
+    private User owner;
+
+    @ElementCollection
+    @Column(nullable = false, name = "attendeeName", length = 255)
+    private List<String> attendeeNames;
 
     @Column(nullable = false, name = "createdAt", length = 255)
     private LocalDateTime createdAt;
