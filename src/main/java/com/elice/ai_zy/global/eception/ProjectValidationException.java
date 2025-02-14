ecb164dfbae4e3aa8acae752683e2174949bcad5
LeeTaeJung <nerostarin@naver.com>
2025-02-14 10:39:32 +0900

    feat: ExceptionHandler & fix: projectPostTest

---
+++ src/main/java/com/elice/ai_zy/global/eception/ProjectValidationException.java
@@ -0,0 +1,7 @@
+package com.elice.ai_zy.global.eception;
+
+public class ProjectValidationException extends RuntimeException{
+    public ProjectValidationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/elice/ai_zy/global/handler/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.elice.ai_zy.global.handler;
+
+import com.elice.ai_zy.global.eception.ProjectValidationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.validation.FieldError;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(ProjectValidationException.class)
+    public ResponseEntity<Map<String, String>> handleProjectValidationException(
+            ProjectValidationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", ex.getMessage());
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
